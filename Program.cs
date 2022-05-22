bd4896798396e16d8ae689dca3596e3a54a9d4c4
contihan <daniel.hanak@outlook.cz>
2022-05-22 20:39:20 +0200

    add as windows service

--- Program.cs
+++ Program.cs
@@ -1,41 +1,66 @@
-﻿using System.Globalization;
+﻿using Topshelf;
 using WeatherInformation.model;
+using WeatherInformation.service;
 
 //string apiKey = "a7bf257916dee56198a99ce95aaef81d";
 //string city = "Hradec Králové";
 //string url = string.Format("https://api.openweathermap.org/data/2.5/weather?q={0}&APPID={1}&units=metric", city, apiKey);
 //string filePath = @"..\..\..\data.txt";
 
-DataHandler dataHandler = new();
-DataHandler dataHandlerPraha = new(city: "Praha");
-DataHandler dataHandlerLiberec = new(city: "Liberec");
-DataHandler dataHandlerHribojedy = new(city: "Hřibojedy");
-WeatherInfo.Root weatherData = dataHandler.GetWeatherDataAsObject();
+//DataHandler dataHandler = new();
+//DataHandler dataHandlerPraha = new(city: "Praha");
+//DataHandler dataHandlerLiberec = new(city: "Liberec");
+//DataHandler dataHandlerHribojedy = new(city: "Hřibojedy");
+//WeatherInfo.Root weatherData = dataHandler.GetWeatherDataAsObject();
 
-Console.WriteLine(weatherData.FullInfo);
+//Console.WriteLine(weatherData.FullInfo);
 
-TextFile dataManager = new();
-dataManager.Save(weatherData, @"..\..\..\pokus_textfile.txt");
-Console.WriteLine();
+//TextFile dataManager = new();
+//dataManager.Save(weatherData, @"..\..\..\pokus_textfile.txt");
+//Console.WriteLine();
 
-weatherData = dataHandlerPraha.GetWeatherDataAsObject();
-Console.WriteLine($"City: {weatherData.Name}");
-Console.WriteLine(dataHandlerPraha.GetWeatherDataAsCsv());
-dataManager.AppendAndSave(weatherData, @"..\..\..\pokus_textfile.txt");
-Console.WriteLine();
+//weatherData = dataHandlerPraha.GetWeatherDataAsObject();
+//Console.WriteLine($"City: {weatherData.Name}");
+//Console.WriteLine(dataHandlerPraha.GetWeatherDataAsCsv());
+//dataManager.AppendAndSave(weatherData, @"..\..\..\pokus_textfile.txt");
+//Console.WriteLine();
 
-weatherData = dataHandlerLiberec.GetWeatherDataAsObject();
-Console.WriteLine($"City: {weatherData.Name}");
-Console.WriteLine(dataHandlerLiberec.GetWeatherDataAsCsv());
-dataManager.AppendAndSave(weatherData, @"..\..\..\pokus_textfile.txt");
-Console.WriteLine();
+//weatherData = dataHandlerLiberec.GetWeatherDataAsObject();
+//Console.WriteLine($"City: {weatherData.Name}");
+//Console.WriteLine(dataHandlerLiberec.GetWeatherDataAsCsv());
+//dataManager.AppendAndSave(weatherData, @"..\..\..\pokus_textfile.txt");
+//Console.WriteLine();
 
-weatherData = dataHandlerHribojedy.GetWeatherDataAsObject();
-Console.WriteLine($"City: {weatherData.Name}");
-Console.WriteLine(dataHandlerHribojedy.GetWeatherDataAsCsv());
-dataManager.AppendAndSave(weatherData, @"..\..\..\pokus_textfile.txt");
-Console.WriteLine();
+//weatherData = dataHandlerHribojedy.GetWeatherDataAsObject();
+//Console.WriteLine($"City: {weatherData.Name}");
+//Console.WriteLine(dataHandlerHribojedy.GetWeatherDataAsCsv());
+//dataManager.AppendAndSave(weatherData, @"..\..\..\pokus_textfile.txt");
+//Console.WriteLine();
 
-Console.WriteLine(new DataHandler(city: "Brandýs nad Labem").GetWeatherDataAsCsv());
-new SqlDataAccess().AddToSqlDb(weatherData);
-Console.WriteLine();
+//Console.WriteLine(new DataHandler(city: "Brandýs nad Labem").GetWeatherDataAsCsv());
+//new SqlDataAccess().AddToSqlDb(weatherData = dataHandlerLiberec.GetWeatherDataAsObject());
+//Console.WriteLine();
+
+WinService();
+
+static void WinService()
+{
+    var exitCode = HostFactory.Run(x =>
+    {
+        x.Service<WeatherInfoService>(s =>
+        {
+            s.ConstructUsing(WeatherInfo => new WeatherInfoService());
+            s.WhenStarted(WeatherInfo => WeatherInfo.Start());
+            s.WhenStopped(WeatherInfo => WeatherInfo.Stop());
+        });
+
+        x.RunAsLocalSystem();
+
+        x.SetServiceName("WeatherInfoService");
+        x.SetDisplayName("WeatherInfo Service");
+        x.SetDescription("This service logs temperature from weather API to local database");
+    });
+
+    int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+    Environment.ExitCode = exitCodeValue;
+}
---
+++ service/WeatherInfoService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherInformation.model;
+
+namespace WeatherInformation.service
+{
+    internal class WeatherInfoService
+    {
+        private readonly System.Timers.Timer timer;
+
+        public WeatherInfoService()
+        {
+            timer = new(5 * 1000) { AutoReset = true };
+            timer.Elapsed += TimerElapsed;
+        }
+
+        private void TimerElapsed(object? sender, System.Timers.ElapsedEventArgs e)
+        {
+            new SqlDataAccess().AddToSqlDb(new DataHandler(city: "Hradec Králové").GetWeatherDataAsObject());
+        }
+
+        public void Start() { timer.Start(); }
+        public void Stop() { timer.Stop(); }
+    }
+}
